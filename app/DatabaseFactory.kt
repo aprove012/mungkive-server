30cfc3a54910ddaaef38229e6be4e3fd621b4c71
aprove <34180766+aprove012@users.noreply.github.com>
2025-06-03 19:16:02 +0900

    [chore] #9 - post, comment 테이블에 attritbute(userPic, locName) 추가

--- app/DatabaseFactory.kt
+++ app/DatabaseFactory.kt
@@ -26,9 +26,11 @@ object DatabaseFactory {
                     id       INTEGER PRIMARY KEY AUTOINCREMENT,
                     userId   TEXT NOT NULL,
                     userName TEXT NOT NULL,
+                    userPic  TEXT NOT NULL,
                     content  TEXT NOT NULL,
                     picture  TEXT NOT NULL,
                     locate   TEXT NOT NULL,
+                    locName  TEXT NOT NULL,
                     likes     INTEGER NOT NULL DEFAULT 0
                 );
                 """
@@ -39,6 +41,7 @@ object DatabaseFactory {
                     id       INTEGER PRIMARY KEY AUTOINCREMENT,
                     postId   INTEGER NOT NULL,
                     userId   TEXT NOT NULL,
+                    userPic  TEXT NOT NULL,
                     content  TEXT NOT NULL,
                     created  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                     FOREIGN KEY(postId) REFERENCES posts(id),
